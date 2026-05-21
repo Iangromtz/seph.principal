a51aa7daf429f6c1ce6a77812508c6ae68ae1be4
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2026-05-21 14:53:52 -0600

    feat:agregamos las clase que se encargan de la respuesta hacia el cliente.

---
+++ Seph.Principal.Application/Common/Models/ResponseFactory.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+
+namespace Seph.Principal.Application.Common.Models
+{
+    public static class ResponseFactory
+    {
+        public static ResponseWrapper<T> Success<T>(T data, string message = "Operación completada correctamente", HttpStatusCode statusCode = HttpStatusCode.OK)
+            => new()
+            {
+                StatusCode = statusCode,
+                Message = message,
+                Data = data
+            };
+
+        public static ResponseWrapper<T> Failure<T>(string message, HttpStatusCode statusCode)
+            => new()
+            {
+                StatusCode = statusCode,
+                Message = message,
+                Data = default!
+            };
+    }
+
+}
---
+++ Seph.Principal.Application/Common/Models/ResponseWrapper.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace Seph.Principal.Application.Common.Models
+{
+    /*Esta clase se encarga de generar la respuetsa del proceso generico*/
+    public class ResponseWrapper<T>
+    {
+        public HttpStatusCode StatusCode { get; set; }
+
+        public string Message { get; set; }
+
+        public T Data { get; set; }
+    }
+}
