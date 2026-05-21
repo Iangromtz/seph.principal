8b40d83b540ffde5380dfbb73ec6ffaf8fcdade1
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2026-05-21 14:32:36 -0600

    feat:se generan todos los validadores  dentro de la capa de aplicacion utilizando los pipedline de mediatr

--- Seph.Principal.Application/Common/Behaviors/ValidationBehavior.cs
+++ Seph.Principal.Application/Common/Behaviors/ValidationBehavior.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Seph.Principal.Application.Common.Behaviors
 {
-    internal class ValidationBehavior
+    /*Esta clase no se puede heredar, utiliza un pipedLine de MediaTr y recive un request general
+     *donde en esta peticion request generica puede ser un command/ query, y un response generica
+     *que puede ser un response de un command o un response de un query
+     */
+    public sealed class ValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validatos):IPipelineBehavior<TRequest, TResponse>
+        where TRequest : notnull 
     {
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (!validatos.Any())
+            {
+                return await next();
+            }
+            ///recibimos todo el contexto en general , y lo validamos, si no es valido lanzamos una 
+            ///excepcion de validacion, si es valido continuamos con el siguiente comportamiento
+            var context = new ValidationContext<TRequest>(request);
+            var failures = validatos
+                .Select(validator => validator.Validate(context))
+                .SelectMany(result => result.Errors)
+                .Where(error => error is not null)
+                .ToList();
+            //si hay errores de validacion, lanzamos una excepcion de validacion con los errores encontrados
+            if (failures.Count != 0)
+            {
+                throw new ValidationException(failures);
+            }
+            return await next();
+        }
     }
 }
