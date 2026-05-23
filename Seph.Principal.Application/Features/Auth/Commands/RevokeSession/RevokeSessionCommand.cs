6797ad1b535e625c997d53faadff7755ea56c415
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2026-05-23 01:18:00 -0600

    feat:se agrega el revokeSessionComand para trabajar con el comando de refresh del token.

---
+++ Seph.Principal.Application/Features/Auth/Commands/RevokeSession/RevokeSessionCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using Seph.Principal.Application.Common.Models;
+
+namespace Seph.Principal.Application.Features.Auth.Commands.RevokeSession
+{
+    public sealed record RevokeSessionCommand(string RefreshToken):IRequest<ResponseWrapper<bool>>;
+   
+}
