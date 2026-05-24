5e0d326692adc3390d22d8593dd5ea1796e2a037
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2026-05-24 16:22:01 -0600

    feat:agregamos la configuracion base para trabajar con los roles de la aplicacion utilizando y heredando de IdentityRole

--- Seph.Principal.Infraestructure/Identity/ApplicationRole.cs
+++ Seph.Principal.Infraestructure/Identity/ApplicationRole.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity;
 
 namespace Seph.Principal.Infraestructure.Identity
 {
-    public class ApplicationRole
+    public sealed  class ApplicationRole : IdentityRole<Guid>
     {
+        public string Description { get; set; } = string.Empty;
     }
 }
