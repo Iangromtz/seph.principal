9e5d78c809d8fafec9e712c5eb89c848e7fdb30c
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2026-06-03 00:51:19 -0600

    feat:agregamos la nueva entidad institucion a las validaciones necesarias ,en la arquitectura del proyecto.

---
+++ Seph.Principal.Infraestructure/Persistence/Configurations/InstitucionConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Seph.Principal.Domain.Entities;
+
+namespace Seph.Principal.Infraestructure.Persistence.Configurations
+{
+    public sealed class InstitucionConfiguration : IEntityTypeConfiguration<Institucion>
+    {
+        public void Configure(
+        EntityTypeBuilder<Institucion> builder)
+        {
+            builder.ToTable("Instituciones");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(x => x.StrValor)
+                .HasMaxLength(150)
+                .IsRequired();
+
+            builder.Property(x => x.StrDescripcion)
+                .HasMaxLength(450);
+        }
+    }
+}
