2e7b9a0adbe610c8e094371d9c2e9e4d86d1ced2
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2026-06-03 02:19:09 -0600

    feat:agregamos  el caso GetInstitucionesQuery

---
+++ Seph.Principal.Application/Features/Instituciones/Queries/GetInstituciones/GetInstitucionesQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using Seph.Principal.Application.Common.Models;
+
+namespace Seph.Principal.Application.Features.Instituciones.Queries.GetInstituciones
+{
+    public sealed record GetInstitucionesQuery()
+     : IRequest<ResponseWrapper<IReadOnlyList<InstitucionDto>>>;
+}
